8c7b744f2a40c2510502922cbe81366baab03606
Maaz Nonsola <maaznonsola786@gmail.com>
2023-05-13 12:18:17 +0530

    first major commit

---
+++ src/components/common/like.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+class Like extends Component {
+  render() {
+    let classes = "fa fa-heart";
+    if (!this.props.liked) {
+      classes += "-o";
+    }
+    return (
+      <i
+        className={classes}
+        onClick={this.props.onLikeToggle}
+        aria-hidden="true"
+        style={{cursor: "pointer"}}
+      ></i>
+    );
+  }
+}
+
+export default Like;
