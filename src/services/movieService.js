dad1e5920a52cabca044134bc5f0ce6bf909fe00
Maaz Nonsola <maaznonsola786@gmail.com>
2023-07-15 12:33:38 +0530

    Saving the Movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -11,7 +11,14 @@ export function getMovie(id) {
   return http.get(apiEndpoint + "/" + id);
 }
 
-export async function saveMovie(movie) {}
+export async function saveMovie(movie) {
+  if (movie._id) {
+    const body = {...movie};
+    delete body._id;
+    return http.put(apiEndpoint + "/" + movie._id, body);
+  }
+  return http.post(apiEndpoint, movie);
+}
 
 export function deleteMovie(id) {
   return http.delete(apiEndpoint + "/" + id);
